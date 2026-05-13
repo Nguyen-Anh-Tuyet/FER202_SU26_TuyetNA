822bc856fe7b3ef7379647ee219e66a8b6c96db8
Nguyen Anh Tuyet <atuyet129129@gmail.com>
2026-05-13 16:47:27 +0700

    demo arrow function

--- Slot2/Exercises/ex2/src/App.js
+++ Slot2/Exercises/ex2/src/App.js
@@ -2,23 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //Khai bao ham Chao1 nhận 1 tham số năme, in ra lời chào
+  //tên đó ở console
+  let chao1=({name})=>{
+    console.log(`Xin chào ${name}`);
+}//gọi hàm tên
+chao1({name:"Tuyết"});
+// viết hàm chao2 nhận 1 tham số là 1 đối tượng person
+//khai báo 1 đối tuọng person có các thuộc tính id,name,age,address
+let person={
+    id:1,
+    name:"Tuyết",
+    age:19,
+    address:"Quảng Ngãi"
+}
+//in thông tin của đối tượng person ra the Card trong React
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <h1>Xin chào mọi người</h1>
+   <h2>Xin chào Tuyết</h2>
+   <h3>Xin chào Tuyết, bạn 19 tuổi, sống tại Quảng Ngãi</h3>
+   <button onClick={()=>chao1({name:"Tuyết"})}>Chào 1</button>
+   <div className="card">
+    <h2>{person.name}</h2>
+    <p>Tuổi: {person.age}</p>
+    <p>Địa chỉ: {person.address}</p>
+    
+   </div>
+   </>  
   );
 }
 
